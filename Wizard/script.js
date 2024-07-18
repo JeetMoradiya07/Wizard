05562d0a839d6d730b6c367fbe892aca89253aef
JeetMoradiya07 <jeet.moradiya.00@gmail.com>
2024-07-18 15:19:50 +0530

    Update

--- Wizard/script.js
+++ Wizard/script.js
@@ -2,9 +2,9 @@ let navIcon = document.getElementById("nav-icon");
 let navbarElement = document.getElementById("navbar-ul");
 
 navIcon.addEventListener("click", function () {
-    if (navbarElement.style.display === "block") {
-        navbarElement.style.display = "none";
-    } else {
-        navbarElement.style.display = "block";
-    }
+  if (navbarElement.style.display === "block") {
+    navbarElement.style.display = "none";
+  } else {
+    navbarElement.style.display = "block";
+  }
 });
